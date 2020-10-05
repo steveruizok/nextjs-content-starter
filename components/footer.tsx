f28a708d02be5e86362abb09f936e34f0cfc00c4
Steve <steveruizok@gmail.com>
2020-10-05 21:48:03 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,13 @@
-import { A, Divider, Text } from "./theme"
+import { A, Text } from "./theme"
 
 export default function Footer() {
   return (
     <footer>
       <Text css={{ mt: "$8" }} variant="detail">
-        © 2020 <A href="https://twitter.com/steveruizok">Steve Ruiz</A>
+        © 2020 <A href="https://twitter.com/steveruizok">Steve Ruiz</A> •{" "}
+        <A href="https://github.com/steveruizok/nextjs-content-starter">
+          Github
+        </A>
       </Text>
     </footer>
   )
